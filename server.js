0f8bff5e84f2e932f12af1d4e5dc08838e6f6ef9
Jean Carlos Rodriguez <jc_rc1812@hotmail.com>
2016-03-24 17:12:04 -0400

    add some code

--- server.js
+++ server.js
@@ -28,6 +28,49 @@ app.get('/About',middleware.requireAuthentication,function(req,res){
 //si no espacificamos la ruta por defecto toma index.html
 app.use(express.static(__dirname + '/public'))
 
+var todos = [{
+    id:1,
+    description:"Meet mom for lunch",
+    completed : false
+},{
+  id:2,
+  description:"Go to market",
+  completed:false
+},{
+    id:3,
+    description: "Call Jorge",
+    completed:true
+}];
+
+app.get('/',function(req,res) {
+    res.send('TODO API Root')
+});
+
+//GET /todos
+app.get('/todos',function(req,res){
+    res.json(todos);
+})
+
+//GET /todos/:id = /todos/1
+app.get('/todos/:id',function(request,response){
+    
+    var todoId = parseInt(request.params.id);
+    var todoEntity;
+    todos.forEach(function(todo) {
+        if(todo.id === todoId){
+            todoEntity = todo
+        }
+    });
+    if(todoEntity){
+        console.log(todoEntity);
+            response.json(todoEntity);
+    }else{
+        response.status(404).send()
+    }
+
+    //response.send('asking for todo' + request.params.id)
+});
+
 //puerto,
 app.listen(port,function() {
     console.log("Express server started on port : " + port)
